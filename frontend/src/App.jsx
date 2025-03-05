6dc5f00b5343802f39f4737f6a17dc545848cac0
mrshivam77 <mrshivamraj77@gmail.com>
2025-03-05 19:42:25 +0530

    deleting fronend and backend

--- frontend/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import TrafficVisualization from './traffic_visualization'
-function App() {
-  return (
-    <div><TrafficVisualization /></div>
-  )
-}
-
-export default App
